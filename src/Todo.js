2f84c6bf2dac3e8b3194a059182799ddf8c1ba08
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-09-28 16:19:24 -0300

    created

---
+++ src/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Todo.css'
+
+function Todo() {
+    return(
+        <div className='container'>
+             <h1>Todo App</h1>
+             <form>
+                <input type='text'></input>
+                <button>Add</button>
+             </form>
+
+             <ul>
+                <li>Item</li>
+             </ul>
+        </div>
+       
+    )
+}
+
+export default Todo
